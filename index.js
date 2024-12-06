803518f7820bb7615ac22ed9d61d33b9ee51645b
Vincent-Carette <vc39108@pausd.us>
2024-12-06 10:20:47 -0800

    8u0gerijog3rpj349jg9g349g43j09k0940k93gk09

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ let equations = document.querySelectorAll(".equation")
 for (let i = 0; i < equations.length; i++) {
     equations[i].input = function(event) {
         alert(event.data)
+        alert(event.data.length)
     }
 }
